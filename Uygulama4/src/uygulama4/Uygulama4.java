8584c1e866177a9b20a2fea4fa68f64cf3de9788
Rabiya2020 <65968900+Rabiya2020@users.noreply.github.com>
2020-06-05 18:12:45 +0300

    Add files via upload

---
+++ Uygulama4/src/uygulama4/Uygulama4.java
@@ -0,0 +1,21 @@
+
+package uygulama4;
+
+
+public class Uygulama4 {
+
+   public static void main(String[] args) 
+    {
+        böl();
+        //Ana metodumuz olan main metodumuzun içerisinde işlemimiizin sonucunu ekrana yazdırıyor.
+    }
+    
+    static void böl()
+    //Geriye değer döndürmeyen ve parametresiz böl adında metod oluşturduk.
+    {
+        System.out.println("Sonuç = "+ 25/5);
+        //Bu metod 25'i 5 e bölerek ekrana yazdırıyor.
+    }
+        
+        
+    }
